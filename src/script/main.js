c44cb0ad1f8857c7cfbbc28615776f56ad20cafb
Krzysiek <krzysztof.luuczak@gmail.com>
2023-10-15 20:36:04 +0200

    add prounancation of words

--- src/script/main.js
+++ src/script/main.js
@@ -2,19 +2,13 @@
 import translate from "../../node_modules/translate/index.min.js"
 import uiInit from "./ui.js"
 import getRandomWord from "./getWord.js"
+import speak from "./speak.js"
+
 
 translate.from ="eng"
 const trans  = async(from)=>{
 let trans = await translate(`${from}`, 'pol')
 return trans
-}
-const getMp3 = (word)=>{
-    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`).then(resp=>{return resp.json()}).then(resp=>{
-       let [arr] = resp
-       console.log(arr);
-       
-        
-    })
 }
 
 const displayTranslation =(translation)=>{
@@ -26,10 +20,14 @@ cardTranslation.innerText = translation
 const init = () =>{
     uiInit()
     getRandomWord().then(resp=>{
-        getMp3(resp)
+        // getMp3(resp)
         return trans(resp)
     }).then((resp)=>{
         displayTranslation(resp)
     })
 }
 init()
+
+
+
+speak()
